6851d7d628108529b4eca91a5cc13dede46f18d5
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-03-04 13:31:10 +0300

    Added redis prefixes

--- chd/celery.py
+++ chd/celery.py
@@ -1,5 +1,6 @@
 import os
 
+from django.conf import settings
 from celery import Celery
 
 # Set the default Django settings module
@@ -9,6 +10,8 @@ app = Celery("chd")
 
 # Load task modules from all registered Django apps
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.result_key_prefix = settings.DB_PREFIX
 
 # Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
